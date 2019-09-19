1ff01e16ac831e49ef0a7e99a3beece9cf0c7352
Sieg <siegblink@gmail.com>
2019-09-19 13:06:23 +0800

    Add Search component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import Search from './components/users/Search'
 import axios from 'axios'
 import './App.css'
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
   }
 
   async componentDidMount() {
-    this.setState({loading: true})
-    const response = await axios.get('https://api.github.com/users')
+    this.setState({ loading: true })
+    const response = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
     this.setState({ users: response.data, loading: false })
   }
 
@@ -21,6 +24,7 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Search />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
---
+++ src/components/users/Search.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export class Search extends Component {
+  state = {
+    text: '',
+  }
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  onSubmit = event => {
+    event.preventDefault()
+    console.log(this.state.text)
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit} className='form'>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search Users...'
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input type='submit' value='Search' className='btn btn-dark btn-block' />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Search
