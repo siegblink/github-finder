208f2b0d88012e40071608f155ae25d19cfaeaa0
Sieg <siegblink@gmail.com>
2019-09-19 14:39:45 +0800

    Add client-side routing

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 function Navbar({ icon, title }) {
   return (
@@ -7,6 +8,14 @@ function Navbar({ icon, title }) {
       <h1>
         <i className={icon}></i> {title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </nav>
   )
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+export default function About() {
+  return (
+    <Fragment>
+      <h1>About This App</h1>
+      <p>App to search Github users</p>
+      <p>Version: 1.0.0</p>
+    </Fragment>
+  )
+}
